35924349c126aad7a84807bd0cf64226c3f76b43
rxxuzi <rxxuzi@gmail.com>
2024-03-06 23:22:39 +0900

    Update: Multiple video downloads handled in parallel.

--- src/api.py
+++ src/api.py
@@ -2,34 +2,55 @@ import argparse
 import sys
 import validators
 from downloader import download_video, download_audio
+from concurrent.futures import ThreadPoolExecutor
+from path import output_path
 
+default_quality = 0
 
 def main():
-    parser = argparse.ArgumentParser(description='Download videos or audio from YouTube.')
-    parser.add_argument('url', type=str, help='The URL of the video to download.')
-    parser.add_argument('output', nargs='?', default=None, help='The output filename (optional).')
+    parser = argparse.ArgumentParser(description='Download videos or audio from multiple URLs in parallel.')
+    parser.add_argument('urls', nargs='+', type=str, help='The URLs of the videos to download.')
     parser.add_argument('-a', '--audio', action='store_true', help='Download audio only.')
     parser.add_argument('-v', '--video', action='store_true', help='Download video. This is the default action.')
 
     args = parser.parse_args()
 
-    # Validate URL
-    if not validators.url(args.url):
-        print("The provided URL is not valid.")
+    # Validate URLs
+    invalid_urls = [url for url in args.urls if not validators.url(url)]
+    if invalid_urls:
+        print(f"The following URLs are not valid: {', '.join(invalid_urls)}")
         sys.exit(2)
 
     if not args.audio and not args.video:
         print("You must specify either video or audio download.")
         sys.exit(3)
 
-    try:
+    # Download in parallel
+    with ThreadPoolExecutor() as executor:
+        futures = []
         if args.audio:
-            download_audio(args.url, args.output)
-        else:
-            download_video(args.url, args.output)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
+            for url in args.urls:
+                futures.append(executor.submit(download_audio, url, None,output_path))
+        elif args.video:
+            for url in args.urls:
+                futures.append(executor.submit(download_video, url, default_quality, None,output_path))
+
+    # Collect results from all futures
+    results = [future.result() for future in futures]
+
+    print(results)
+
+    # Calculate the sum of the values in the `result` key
+    sum_result: int = sum(result['result'] for result in results)
+
+    # Compile the values (file names) of the output key into a list
+    output_files = [result['output'] for result in results if result['result'] == 0]
+
+    print("Successfully downloaded files:")
+    for output_file in output_files:
+        print(output_file)
+
+    sys.exit(sum_result)
 
 
 if __name__ == '__main__':
--- src/downloader.py
+++ src/downloader.py
@@ -2,7 +2,14 @@ import yt_dlp
 from path import ffmpeg_path, output_path
 
 
-def download_video(url, quality, output_filename=None, output_dir=output_path, ):
+def download_video(url, quality, output_filename=None, output_dir=output_path):
+    output = None
+
+    def hook(d):
+        nonlocal output
+        if d['status'] == 'finished':
+            output = d['filename']
+
     format_option = 'bestvideo+bestaudio/best'
     if quality == 1:
         format_option = 'bestvideo[height<=720]+bestaudio/best[height<=720]'
@@ -19,21 +26,29 @@ def download_video(url, quality, output_filename=None, output_dir=output_path, )
     ydl_opts = {
         'format': format_option,
         'outtmpl': outtmpl,
-        'ffmpeg_location': ffmpeg_path,  # `ffmpeg`のパスを指定
+        'ffmpeg_location': ffmpeg_path,
+        'progress_hooks': [hook],
         'quiet': True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
             ydl.download([url])
-
-            print("Download is complete.")
-
         except Exception as e:
-            print(f"An error occurred during download: {e}")
+            print(f"An error occurred during download: {e} \n file : {output}")
+            return {"result": 1, "output": output}
+
+        return {"result": 0, "output": output}
 
 
-def download_audio(url, output_filename=None, output_dir=output_path, ):
+def download_audio(url, output_filename=None, output_dir=output_path):
+    output = None
+
+    def hook(d):
+        nonlocal output
+        if d['status'] == 'finished':
+            output = d['filename']
+
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': f'{output_dir}/{output_filename if output_filename else "%(title)s"}',
@@ -43,6 +58,7 @@ def download_audio(url, output_filename=None, output_dir=output_path, ):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
+        'progress_hooks': [hook],
         'quiet': True,
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -51,3 +67,6 @@ def download_audio(url, output_filename=None, output_dir=output_path, ):
             print("Download is complete.")
         except Exception as e:
             print(f"An error occurred during download: {e}")
+            return {"result": 1, "output": output}
+
+        return {"result": 0, "output": output}
